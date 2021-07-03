542e35d78cdd7bf7982228ee40442c34593ebb96
crisOspina <crisospina523@gmail.com>
2021-07-02 22:45:57 -0500

    main page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import 'tailwindcss/tailwind.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
+import type {AppProps} from 'next/app'
+
+function MyApp({Component, pageProps}: AppProps) {
   return <Component {...pageProps} />
 }
 export default MyApp
